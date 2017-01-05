bd4e64182c853e806250ddaefae9fe9bb85256b3
Elton Santana <efs9@bepid.cin.ufpe.br>
2017-01-05 19:45:01 -0200

    Removing xib strategy and adding all view controllers and their classes

---
+++ PingPong/PingPong/VIewControllers/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  PingPong
+//
+//  Created by Elton Santana on 05/01/17.
+//  Copyright © 2017 Back St Eltons. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func PlayButtonAction(_ sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
