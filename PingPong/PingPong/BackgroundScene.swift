e564c2276474b43a16c4db1129542c644fb5db0e
Elton Santana <efs9@bepid.cin.ufpe.br>
2017-01-18 10:16:17 -0300

    Creating files for background animation

---
+++ PingPong/PingPong/BackgroundScene.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundScene.swift
+//  PingPong
+//
+//  Created by Elton Santana on 16/01/17.
+//  Copyright © 2017 Back St Eltons. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class BackgroundScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        
+        
+    }
+    
+}
--- PingPong/PingPong/VIewControllers/HomeViewController.swift
+++ PingPong/PingPong/VIewControllers/HomeViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import GameplayKit
+import SpriteKit
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.presentSKView()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +30,29 @@ class HomeViewController: UIViewController {
     
     @IBAction func PlayButtonAction(_ sender: UIButton) {
         self.showConnectionPopUp()
+    }
+    
+    func presentSKView(){
+        
+        if let scene = GKScene(fileNamed: "BackgroundScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! BackgroundScene? {
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .fill
+                                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
     }
     
     func showConnectionPopUp(){
